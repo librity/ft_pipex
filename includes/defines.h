db1dcc637f5fc983f57e88b36d3b9898e86ade73
Luis Geniole <luis.geniole@gmail.com>
2022-03-23 15:42:23 -0300

    Update libft.

--- includes/defines.h
+++ includes/defines.h
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 18:20:45 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/03/12 18:32:10 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/03/21 11:42:39 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@
 # define HELP_MSG "Welcome to lpaulo-m's pipex!\n\
 \n\
 USAGE:\n\
-	./pipex file1 cmd1 cmd2 file2\n\
+	./pipex file1 \"cmd1\" \"cmd2\" file2\n\
 \n\
 EXAMPLES:\n\
 	./pipex infile \"ls -l\" \"wc -l\" outfile\n\
--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 21:58:19 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/03/11 19:58:26 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/03/22 22:23:29 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,8 +107,6 @@ int					ft_lerp_wsteps_i(int from, int to, int steps, int x);
  * MEMORY
 \******************************************************************************/
 
-# define MALLOC_ERROR_MESSAGE "ERROR: Unable to allocate required memory.\n"
-
 void				*ft_salloc(size_t size);
 void				*ft_memset(void *s, int c, size_t n);
 void				ft_bzero(void *s, size_t n);
@@ -179,6 +177,8 @@ char				*ft_strjoin_del_del(char **delete_me, char **delete_me_too);
 
 void				ft_free_string_array(char **free_me);
 
+bool				ft_starts_with(char *string, char *prefix);
+
 /******************************************************************************\
  * STRING TO NUMBER
 \******************************************************************************/
@@ -232,12 +232,6 @@ void				ft_putstr_array(char **strings);
 /******************************************************************************\
  * PRINT NUMBERS
 \******************************************************************************/
-
-# define DEFAULT_PRINT_FLOAT_PRECISION 6
-
-# define DECIMAL_BASE "0123456789"
-# define LOWERCASE_HEX_BASE "0123456789abcdef"
-# define UPPERCASE_HEX_BASE "0123456789ABCDEF"
 
 bool				ft_is_valid_base(const char *base,
 						const size_t base_length);
@@ -299,10 +293,19 @@ unsigned int		ft_count_digits_d(double number, int precision);
 unsigned int		ft_count_chars_ld(long double number, int precision);
 unsigned int		ft_count_digits_ld(long double number, int precision);
 
-char				*ft_skip_digits(char *digits);
-char				*ft_skip_number(char *digits);
-char				*ft_skip_whitespace(char *digits);
-char				*ft_skip_plus_or_minus(char *digits);
+char				*ft_skip_digits(char *str);
+char				*ft_skip_whitespace(char *str);
+char				*ft_skip_plus_or_minus(char *str);
+char				*ft_skip_commas(char *str);
+
+char				*ft_skip_number(char *str);
+char				*ft_skip_int(char *str);
+char				*ft_skip_int_commas(char *str);
+char				*ft_skip_int_whitespace(char *str);
+
+char				*ft_skip_float(char *str);
+char				*ft_skip_float_commas(char *str);
+char				*ft_skip_float_whitespace(char *str);
 
 /******************************************************************************\
  * LINKED_LISTS
@@ -330,11 +333,17 @@ int					ft_lstsize(t_list *lst);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+
 void				ft_lst_append(t_list **lst, void *content);
 void				ft_lst_prepend(t_list **lst, void *content);
 
-void				ft_lstdelone(t_list *lst, void (*del)(void *));
-void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lst_add(t_list **list, void *pointer);
+void				ft_lst_create_append(t_list **list, void *pointer);
+void				ft_lst_create_prepend(t_list **list, void *pointer);
+
+void				ft_lst_free(t_list **free_me);
 
 /******************************************************************************\
  * LISTED MEMORY ALLOCATION
@@ -349,11 +358,6 @@ void				ft_free_lalloc(t_list **free_me);
  * FT_GET_NEXT_LINE
 \******************************************************************************/
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 100
-# endif
-# define MAX_FILE_DESCRIPTOR RLIMIT_NOFILE
-
 # define GNL_FOUND_LINEBREAK 1
 # define GNL_FOUND_EOF 0
 # define GNL_ERROR -1
@@ -364,209 +368,73 @@ int					ft_get_next_line(int fd, char **line);
  * FT_PRINTF
 \******************************************************************************/
 
-# ifndef ARG_MAX
-#  define ARG_MAX 131072
-# endif
-
-# define NULL_STRING "(null)"
-
-typedef struct s_printf
-{
-	const char		*format;
-	va_list			elements;
-	int				chars_printed;
-	unsigned int	conversion_position;
-	char			conversion;
-}					t_printf;
-
-typedef struct s_parse_flags
-{
-	char			parsed_flags[ARG_MAX];
-	char			*flags;
-	bool			has_wildcards;
-	bool			is_left_padded_with_zeros;
-	bool			is_left_justified;
-	char			left_padder;
-	bool			has_minimum_width;
-	int				minimum_width;
-	bool			has_precision;
-	int				precision;
-}					t_parse_flags;
-
-typedef struct s_parse_wildcards
-{
-	char			*found_wildcards;
-	unsigned int	conversion_position;
-	int				wildcard;
-	size_t			parser;
-}					t_parse_wildcards;
-
 int					ft_printf(const char *format, ...);
-void				pf_vprintf(t_printf *print_control);
-
-void				pf_initialize_print_control(t_printf *print_control,
-						const char *format);
-void				pf_initialize_flag_control(t_printf *print_control,
-						t_parse_flags *flag_control);
-void				pf_initialize_wildcard_control(t_printf *print_control,
-						t_parse_flags *flag_control,
-						t_parse_wildcards *wildcard_control);
-
-void				pf_parse_flags(t_printf *print_control,
-						t_parse_flags *control);
-void				pf_parse_wildcars(t_printf *print_control,
-						t_parse_flags *flag_control);
-
-bool				pf_handled_no_conversion(t_printf *print_control);
 
 /******************************************************************************\
- * FT_PRINTF PERCENT
+ * COLORS
 \******************************************************************************/
 
-typedef struct s_handle_percent
-{
-	unsigned char	print_me;
-	int				char_count;
-	t_parse_flags	flag_control;
-}					t_handle_percent;
-bool				pf_handled_percent(t_printf *print_control);
-void				pf_print_percent(t_printf *print_control,
-						t_handle_percent *control,
-						t_parse_flags *flag_control);
+/* Black, Red, Green, Yellow, Blue, Purple, Cyan, White */
+# define BK "\033[0;30m"
+# define R "\033[0;31m"
+# define G "\033[0;32m"
+# define Y "\033[0;33m"
+# define B "\033[0;34m"
+# define P "\033[0;35m"
+# define C "\033[0;36m"
+# define W "\033[0;37m"
 
-/******************************************************************************\
- * FT_PRINTF CHAR
-\******************************************************************************/
+/* Bold */
+# define BKB "\033[1;30m"
+# define RB "\033[1;31m"
+# define GB "\033[1;32m"
+# define YB "\033[1;33m"
+# define BB "\033[1;34m"
+# define PB "\033[1;35m"
+# define CB "\033[1;36m"
+# define WB "\033[1;37m"
 
-typedef struct s_handle_c
-{
-	unsigned char	print_me;
-	int				char_count;
-	t_parse_flags	flag_control;
-}					t_handle_c;
-bool				pf_handled_s(t_printf *print_control);
-void				pf_print_c(t_printf *print_control,
-						t_handle_c *control,
-						t_parse_flags *flag_control);
+/* Reset Color */
+# define RC "\033[0m"
 
-/******************************************************************************\
- * FT_PRINTF STRING
-\******************************************************************************/
+void				ft_black(char *str);
+void				ft_black_endl(char *str);
+void				ft_blackb(char *str);
+void				ft_blackb_endl(char *str);
 
-typedef struct s_handle_s
-{
-	char			*print_me;
-	size_t			length;
-	size_t			precision_length;
-	bool			is_null;
-	t_parse_flags	flag_control;
-}					t_handle_s;
-bool				pf_handled_c(t_printf *print_control);
-void				pf_print_s(t_printf *print_control,
-						t_handle_s *control,
-						t_parse_flags *flag_control);
+void				ft_red(char *str);
+void				ft_red_endl(char *str);
+void				ft_redb(char *str);
+void				ft_redb_endl(char *str);
 
-/******************************************************************************\
- * FT_PRINTF INT
-\******************************************************************************/
+void				ft_green(char *str);
+void				ft_green_endl(char *str);
+void				ft_greenb(char *str);
+void				ft_greenb_endl(char *str);
 
-typedef struct s_handle_int
-{
-	long			print_me;
-	int				digit_count;
-	int				char_count;
-	bool			is_negative;
-	bool			is_zero_with_zero_precision;
-	t_parse_flags	flag_control;
-}					t_handle_int;
-bool				pf_handled_int(t_printf *print_control);
-void				pf_print_int(t_printf *print_control,
-						t_handle_int *control,
-						t_parse_flags *flag_control);
+void				ft_yellow(char *str);
+void				ft_yellow_endl(char *str);
+void				ft_yellowb(char *str);
+void				ft_yellowb_endl(char *str);
 
-/******************************************************************************\
- * FT_PRINTF UNSIGNED INT
-\******************************************************************************/
+void				ft_blue(char *str);
+void				ft_blue_endl(char *str);
+void				ft_blueb(char *str);
+void				ft_blueb_endl(char *str);
 
-typedef struct s_handle_u
-{
-	unsigned int	print_me;
-	int				digit_count;
-	bool			is_zero_with_zero_precision;
-	t_parse_flags	flag_control;
-}					t_handle_u;
-bool				pf_handled_u(t_printf *print_control);
-void				pf_print_u(t_printf *print_control,
-						t_handle_u *control,
-						t_parse_flags *flag_control);
+void				ft_purple(char *str);
+void				ft_purple_endl(char *str);
+void				ft_purpleb(char *str);
+void				ft_purpleb_endl(char *str);
 
-/******************************************************************************\
- * FT_PRINTF POINTERS
-\******************************************************************************/
+void				ft_cyan(char *str);
+void				ft_cyan_endl(char *str);
+void				ft_cyanb(char *str);
+void				ft_cyanb_endl(char *str);
 
-typedef struct s_handle_p
-{
-	unsigned long	print_me;
-	int				digit_count;
-	bool			is_zero_with_zero_precision;
-	t_parse_flags	flag_control;
-}					t_handle_p;
-bool				pf_handled_p(t_printf *print_control);
-void				pf_printf_p(t_printf *print_control,
-						t_handle_p *control,
-						t_parse_flags *flag_control);
-
-/******************************************************************************\
- * FT_PRINTF HEXADECIMAL
-\******************************************************************************/
-
-typedef struct s_handle_hex
-{
-	unsigned int	print_me;
-	int				digit_count;
-	bool			is_uppercase;
-	bool			is_zero_with_zero_precision;
-	t_parse_flags	flag_control;
-}					t_handle_hex;
-bool				pf_handled_hex(t_printf *print_control);
-void				pf_printf_hex(t_printf *print_control,
-						t_handle_hex *control,
-						t_parse_flags *flag_control);
-
-/******************************************************************************\
- * FT_PRINTF FLOAT DOUBLE
-\******************************************************************************/
-
-typedef struct s_handle_f
-{
-	double			print_me;
-	int				digit_count;
-	int				char_count;
-	bool			is_negative;
-	bool			is_zero_with_zero_precision;
-	t_parse_flags	flag_control;
-}					t_handle_f;
-bool				pf_handled_f(t_printf *print_control);
-void				pf_print_f(t_printf *print_control,
-						t_handle_f *control,
-						t_parse_flags *flag_control);
-
-/******************************************************************************\
- * FT_PRINTF LONG DOUBLE
-\******************************************************************************/
-
-typedef struct s_handle_lf
-{
-	long double		print_me;
-	int				digit_count;
-	int				char_count;
-	bool			is_negative;
-	bool			is_zero_with_zero_precision;
-	t_parse_flags	flag_control;
-}					t_handle_lf;
-bool				pf_handled_lf(t_printf *print_control);
-void				pf_print_lf(t_printf *print_control,
-						t_handle_lf *control,
-						t_parse_flags *flag_control);
+void				ft_white(char *str);
+void				ft_white_endl(char *str);
+void				ft_whiteb(char *str);
+void				ft_whiteb_endl(char *str);
 
 #endif
